3e8b2b15ccc4602c65b3e34af9deb5fbfbeca60c
abinishifex <abinishifex@gmail.com>
2026-03-09 22:19:05 +0300

    adding moviecard display

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,39 @@ import Navbar from "./components/Navbar"
 import MovieCard from "./components/MovieCard"
 
 function App() {
+  const movies = [
+    {
+    id: 1,
+    title:"Batman", 
+    image: "https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg"
+},
+ {
+    id: 2,
+      title:"Avatar", 
+    image: "https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
+},
+ {
+    id:3,
+      title:"Batman", 
+    image: "https://image.tmdb.org/t/p/w500/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg"
+},
+
+  ]
 
 return(
   <div className="bg-gray-950 min-h-screen">
 
   <Navbar />
-  <MovieCard 
-  title="Batman"
-  image="https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg"
-/>
+  <div className="grid grid-cols-2 md:grid-cols-3 gap-4 p-4 rounded">
+  {movies.map((movie) => (
+    <MovieCard
+    key={movie.id}
+    title={movie.title}
+    image={movie.image}
+    />
+  ))}
+  </div>
+  
   </div>
   )
 
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,12 @@
-function MovieCard({title, image}){
+function MovieCard({id,title, image}){
 return(
-    <div class-lgName="bg-gray-900 rounded-lg overflow-hidden shadow">
+    <div class-lgName="bg-gray-900 rounded overflow-hidden shadow">
 
         <img 
+        key={id}
         src={image} 
         alt={title} 
-        className="w-full"/>
+        className="w-full rounded"/>
 
         
         <h2 className="text-white p-2 text-center">
