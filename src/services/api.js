f4f3714a591a1585efb4c5faeafba8f983473630
abinishifex <abinishifex@gmail.com>
2026-03-10 22:00:17 +0300

    adding movies api integration with TMDB

---
+++ src/services/api.js
@@ -0,0 +1,15 @@
+
+const API_KEY = "33ad0a4fc399c5db95f74efc20a94b18";
+
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const fetchMovies = async() => {
+    
+    const res = await fetch(
+        `${BASE_URL}/trending/movie/week?api_key=${API_KEY}`
+    );
+
+    const data = await res.json();
+
+    return data;
+};
